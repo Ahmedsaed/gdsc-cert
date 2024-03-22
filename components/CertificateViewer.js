741c69220d1c357fa644741657bc08723fa7c9fc
bilal-kun <khawajabilal902@gmail.com>
2024-03-22 20:52:41 +0500

    fixed fonts not being able to render on some machines by rendering canvas and the converting to png

--- components/CertificateViewer.js
+++ components/CertificateViewer.js
@@ -7,37 +7,11 @@ import CertificateTemplate3 from "./cert/CertificateTemplate3";
 import CertificateTemplate4 from "./cert/CertificateTemplate4";
 import Head from "next/head";
 import styles from "../styles/Certificate.module.css";
+import { Canvg } from "canvg";
 
 const saveSvgAsPng = require("save-svg-as-png");
 
 export default function Cert({ params }) {
-    function handleDownloadPNGBtn() {
-        saveSvgAsPng.saveSvgAsPng(
-            document.getElementById("certificate"),
-            "certificate.png",
-            {
-                scale: 1,
-                encoderOptions: 1,
-                backgroundColor: "white",
-                fonts: [
-                    {
-                        url: "https://fonts.gstatic.com/s/karma/v16/va9F4kzAzMZRGLjDY_Z4sK0.woff2",
-                        text: `
-                            @font-face {
-                                font-family: 'Karma';
-                                font-style: normal;
-                                font-weight: 300;
-                                font-display: swap;
-                                src: url(https://fonts.gstatic.com/s/karma/v16/va9F4kzAzMZRGLjDY_Z4sK0.woff2) format('woff2');
-                                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-                            }
-                        `,
-                    },
-                ],
-            }
-        );
-    }
-
     function handleDownloadPDFBtn() {
         const svg = document.getElementById("certificate");
         const width = parseInt(svg.getAttribute("viewBox").split(" ")[2]);
@@ -66,6 +40,36 @@ export default function Cert({ params }) {
                     doc.save("certificate.pdf");
                 });
             });
+    }
+
+    function handleDownloadPNGBtn() {
+        const svg = document.getElementById("certificate");
+        const serializer = new XMLSerializer();
+        const svgStr = serializer.serializeToString(svg);
+        const width = parseInt(svg.getAttribute("viewBox").split(" ")[2]);
+        const height = parseInt(svg.getAttribute("viewBox").split(" ")[3]);
+
+        const canvas = document.createElement('canvas');
+        canvas.width = width
+        canvas.height = height;
+        const ctx = canvas.getContext('2d');
+
+        try {
+
+            const instance = Canvg.fromString(ctx, svgStr)
+            // Renders the SVG into the canvas
+            instance.render().then(() => {
+                const imgData = canvas.toDataURL('image/png');
+                const link = document.createElement('a');
+                link.href = imgData;
+                link.download = "certificate.png";
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            });
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
