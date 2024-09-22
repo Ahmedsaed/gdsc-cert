9740aa5a64144c48ffe028a2ccf01144777918c5
Ahmedsaed <ahmedsaed2652003@gmail.com>
2024-09-23 01:49:34 +0300

    Bug fixes - handle deleted certificates - center text and certificates - handle errors peacefully - remove variables from metadata

--- pages/c/[id].js
+++ pages/c/[id].js
@@ -18,104 +18,139 @@ export default function C(props) {
     }, [router.isReady, router.query.id]);
 
     useEffect(() => {
-        if (typeof window !== "undefined" && id) {
-            firebase
-                .firestore()
-                .collection("cert")
-                .doc(
-                    id.split("-").length > 1
-                        ? id.split("-")[0]
-                        : id.startsWith("omarffhj")
-                        ? "omarffhj"
-                        : id.startsWith("asd")
-                        ? "asd"
-                        : ""
-                ) // This is a hard coded fix for the old certificates. (Original: id.split("-")[0])
-                .collection("core21")
-                .doc(id)
-                .get()
-                .then((doc) => {
-                    setValue(doc.data());
-                });
+        if (typeof window === "undefined" || !id) return;
+
+        if (["IQ55", "334C"].includes(id.split("-")[0])) {
+            // This is a hard coded fix for accounts that were accidentally deleted.
+            setValue({
+                accidentallyDeleted: true,
+            });
+        } else {
+            try {
+                firebase
+                    .firestore()
+                    .collection("cert")
+                    .doc(
+                        id.split("-").length > 1
+                            ? id.split("-")[0]
+                            : id.startsWith("omarffhj")
+                            ? "omarffhj"
+                            : id.startsWith("asd")
+                            ? "asd"
+                            : ""
+                    ) // This is a hard coded fix for the old certificates. (Original: id.split("-")[0])
+                    .collection("core21")
+                    .doc(id)
+                    .get()
+                    .then((doc) => {
+                        const data = doc.data();
+                        if (data)
+                            setValue(doc.data());
+                        else
+                            setValue({
+                                errored: true,
+                            });
+                    })
+                    .catch((e) => {
+                        setValue({
+                            errored: true,
+                        });
+                    });
+            }
+            catch (e) {
+                setValue({
+                    errored: true
+                })
+            }
         }
     }, [id]);
 
-    return value ? (
-        <>
-            <Head>
-                <title>{`${value.name} - GDSC Certificate`}</title>
-                <meta
-                    name="title"
-                    content={`${value.name} - GDSC Certificate`}
-                    key="title"
-                />
-                <meta
-                    name="description"
-                    content="Google Developers Student Clubs Certificate"
-                    key="descripttion"
-                />
-                <meta property="og:type" content="article" key="og:type" />
-                <meta
-                    property="og:url"
-                    content={`https://gdsc-certificates.web.app/c/${id}`}
-                    key="og:url"
-                />
-                <meta
-                    property="og:title"
-                    content={`${value.name} - GDSC Certificate`}
-                    key="og:title"
-                />
-                <meta
-                    property="og:description"
-                    content={`${value.name} - Google Develelopers Student Clubs Certificate`}
-                    key="og:description"
-                />
-                <meta
-                    property="og:image"
-                    content={`https://gdsc-certificates.vercel.app/api/certThumbnail?id=${id}`}
-                    key="og:image"
-                />
-                <meta
-                    property="twitter:card"
-                    content="summary_large_image"
-                    key="twitter:card"
-                />
-                <meta
-                    property="twitter:url"
-                    content={`https://gdsc-certificates.web.app/c/${id}`}
-                    key="twitter:url"
-                />
-                <meta
-                    property="twitter:title"
-                    content={`${value.name} - GDSC Certificate`}
-                    key="twitter:title"
-                />
-                <meta
-                    property="twitter:description"
-                    content={`${value.name} - Google Develelopers Student Clubs Certificate`}
-                    key="twitter:description"
-                />
-                <meta
-                    property="twitter:image"
-                    content={`https://gdsc-certificates.vercel.app/api/certThumbnail?id=${id}`}
-                    key="twitter:image"
-                />
-                <link
-                    rel="canonical"
-                    href="https://gdsc-certificates.web.app/c"
-                    key="canonical"
-                />
-            </Head>
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", textAlign: "center" }}>
+            {value && Object.keys(value).length > 0 && !value.errored ? (
+                <>
+                    <Head>
+                        <title>{`$GDSC Certificate`}</title>
+                        <meta
+                            name="title"
+                            content={`$GDSC Certificate`}
+                            key="title"
+                        />
+                        <meta
+                            name="description"
+                            content="Google Developers Student Clubs Certificate"
+                            key="description"
+                        />
+                        <meta property="og:type" content="article" key="og:type" />
+                        <meta
+                            property="og:url"
+                            content={`https://gdsc-certificates.web.app/c/${id}`}
+                            key="og:url"
+                        />
+                        <meta
+                            property="og:title"
+                            content={`$GDSC Certificate`}
+                            key="og:title"
+                        />
+                        <meta
+                            property="og:description"
+                            content={`$Google Developers Student Clubs Certificate`}
+                            key="og:description"
+                        />
+                        <meta
+                            property="og:image"
+                            content={`https://gdsc-certificates.web.app/api/certThumbnail?id=${id}`}
+                            key="og:image"
+                        />
+                        <meta
+                            property="twitter:card"
+                            content="summary_large_image"
+                            key="twitter:card"
+                        />
+                        <meta
+                            property="twitter:url"
+                            content={`https://gdsc-certificates.web.app/c/${id}`}
+                            key="twitter:url"
+                        />
+                        <meta
+                            property="twitter:title"
+                            content={`$GDSC Certificate`}
+                            key="twitter:title"
+                        />
+                        <meta
+                            property="twitter:description"
+                            content={`$Google Developers Student Clubs Certificate`}
+                            key="twitter:description"
+                        />
+                        <meta
+                            property="twitter:image"
+                            content={`https://gdsc-certificates.vercel.app/api/certThumbnail?id=${id}`}
+                            key="twitter:image"
+                        />
+                        <link
+                            rel="canonical"
+                            href="https://gdsc-certificates.web.app/c"
+                            key="canonical"
+                        />
+                    </Head>
 
-            {id && value ? (
-                <CertificateViewer
-                    params={{ id, ...value }}
-                ></CertificateViewer>
+                    {id && value && value.accidentallyDeleted ? (
+                        <>
+                            This certificate was accidentally deleted. But it was a
+                            valid certificate. If you are the owner of this certificate,
+                            please contact me at mail@ahmedsaed.me
+                        </>
+                    ) : (
+                        <CertificateViewer
+                            params={{ id, ...value }}
+                        />
+                    )}
+                </>
+            ) : !value.errored ? (
+                <div>Loading...</div>
             ) : (
-                <>Loading...</>
+                <div>Not Found</div>
             )}
-        </>
-    ) : (
-        <div>Not Found</div>
+        </div>
     );
 }
